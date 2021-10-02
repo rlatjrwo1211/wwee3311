ea08a553157d29bba02406987f032003e25c1031
rlatjrwo1211 <73834922+rlatjrwo1211@users.noreply.github.com>
2021-10-03 00:39:56 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.on('Ready',()=>{console.log("봇이 준비되었습니다.")})
+client.on('message',msg=>{if(msg.content === '핑'){msg.reply('퐁!')}})
+client.login('ODkzODgyNDQ3MDMxMzMyODg3.YVh65g.D2kUwi0Wk3u-qxE6hXqzubJbA9I')
